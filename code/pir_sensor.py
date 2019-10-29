e6a3e222b8a5a0534437bf81256e788c403e9921
Adam Corbin <acorbin3@gmail.com>
2019-10-29 04:45:30 -0400

    Adding PIR Sensort class to read from the PIR

---
+++ code/pir_sensor.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+
+class PIR_Sensor:
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+
+        self.PIR_PIN = 19
+        GPIO.setup(self.PIR_PIN, GPIO.OUT)
+    
+    # This will return True if the PIR has been triggered and False when no motion
+    def read():
+        return GPIO.input(PIR_PIN)
+        
+    # TODO_AC - Consider looking into GPIO.add_event_detect callback instead of polling
