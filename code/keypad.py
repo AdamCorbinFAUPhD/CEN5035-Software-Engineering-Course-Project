1e3cf5dbb722efc04f2271a92af4fd392ff76a58
Adam Corbin <acorbin3@gmail.com>
2019-11-04 05:19:00 -0500

    Update keypad.py
    Adding system design
    Adding thread to catpture key presses
    Adding queue to put key presses in

--- code/keypad.py
+++ code/keypad.py
@@ -1,47 +1,92 @@
+from queue import Queue
+from threading import Thread
+
 import RPi.GPIO as GPIO
 
-# This class is used to intercept the keypad entries
-# Here are the following steps in order to acheive our matrix 4x4 for all 16 keys
-# 1. Set the columns as outputs to High
-# 2. Set the rows pints as inputs with pull-up resistors
-# 3. While checking for an individual key has been pressed, 1 at a time the output will be set to Low.
-# 4. While the 1 output is set to low, we will check if any of the inputs as set to Low, this will indicate that a button as been pressed
-# 5. In order to avoid sending multiple keypress events while the button is held down, the system will wait till that Low becomes high again
+"""
+This class is used to intercept the keypad entries
+Here are the following steps in order to achieve our matrix 4x4 for all 16 keys
+1. Set the columns as outputs to High
+2. Set the rows pints as inputs with pull-up resistors
+3. While checking for an individual key has been pressed, 1 at a time the output will be set to Low.
+4. While the 1 output is set to low, we will check if any of the inputs as set to Low, this will indicate that a button 
+    as been pressed
+5. In order to avoid sending multiple keypress events while the button is held down, the system will wait till that 
+    Low becomes High again
+    
+System design on how to use the keypad class: 
+https://drive.google.com/file/d/1uPLhlLkEPWDTnYCvWuJ4w6HXwep1SSEe/view?usp=sharing
+
+The Keypad creates its own thread to capture all the key presses and then sends each key press into the queue.
+The intention would be there is a separate thread that would process the keys using the queue
+"""
 
 # Reference: Used this YouTube video on how to connect up the keypad: https://www.youtube.com/watch?v=yYnX5QodqQ4
 class Keypad:
+    """
+    A class used to capture inputs from 4x4 membrane switch
+
+    Attributes
+    ----------
+
+    _row_pins: list[ints]
+        A list of the pin used for inputs for the membrane switch
+    _column_pins: list[ints]
+        A list of the pins used for output columns
+    _matrix: list[list[chars]]
+        A representation of which each button represents on the membrane switch
+    thread: Thread
+        thread in which the parsing of the keypresses will occur and captured
+    keypress_queue: Queue
+        Pass in the key that was pressed in the queue to be processed by a separate thread
+    """
     def __init__(self):
-        # Initilizing the GPIO Mode to Brodcom board pins which is what the T_Extention uses
+        """
+
+        :type
+        :param queue: Pass in the key that was pressed in the queue to be processed by a separate thread
+        """
+
+        # Initializing the GPIO Mode to Brodcom board pins which is what the T_Extension uses
         GPIO.setmode(GPIO.BCM)
-        
-        
-        self._row_pins = [22,23,24,25]
-        self._column_pins = [4,5,6,13]
-        self._matrix = [ ['1','2','3','A'],
-                         ['4','5','6','B'],
-                         ['7','8','9','C'],
-                         ['*','0','#','D'] ]
-        
-        
+
+        self.keypress_queue = Queue()
+        self.thread = Thread(target=self.capture_keypress)
+
+        self._row_pins = [22, 23, 24, 25]
+        self._column_pins = [4, 5, 6, 13]
+        self._matrix = [['1', '2', '3', 'A'],
+                        ['4', '5', '6', 'B'],
+                        ['7', '8', '9', 'C'],
+                        ['*', '0', '#', 'D']]
+
         for col in self._column_pins:
             print("col:", col)
             GPIO.setup(col, GPIO.OUT)
             GPIO.output(col, 1)
-        
+
         for row in self._row_pins:
             print("row:", row)
-            GPIO.setup(row, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-            
-    #TODO_AC - Right now this does not return. Maybe we need to create a seperate thread and a queue to capture the keypresses. 
-    # Something to look into once we have the system all set up.
-    def start(self):
+            GPIO.setup(row, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        # Start monitoring for key presses
+        self.thread.start()
+
+    def capture_keypress(self):
+        """
+        Main processing to capture the keypress and put it into the queue
+
+        """
         while True:
             for j in range(4):
-                GPIO.output(self._column_pins[j],0)
+                GPIO.output(self._column_pins[j], 0)
                 for i in range(4):
                     if GPIO.input(self._row_pins[i]) == 0:
                         print(self._matrix[i][j])
                         # Wait till button becomes unpressed
                         while GPIO.input(self._row_pins[i]) == 0:
                             pass
-                GPIO.output(self._column_pins[j],1)
+                        # add the key that was pressed into the queue
+                        self.keypress_queue.put(self._matrix[i][j])
+                GPIO.output(self._column_pins[j], 1)
+
