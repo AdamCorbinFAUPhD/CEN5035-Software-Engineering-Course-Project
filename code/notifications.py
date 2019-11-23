0f15f61c69e59d9c7f13888a2f7ee552bef69bce
Adam Corbin <acorbin3@gmail.com>
2019-11-23 03:56:20 -0500

    Updating API to pass in a file path Fixing uploading file and getting returned URL adding time as part of the text message

--- code/notifications.py
+++ code/notifications.py
@@ -1,12 +1,14 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
+from datetime import datetime
 import logging
 import base64
+
 import requests
 
 from twilio.rest import Client
 
 # The Notifications secret will live on the server and not in the repo
-from notifications_secret import corbin_from_number, corbin_account_sid, corbin_test_auth_token
+from notifications_secret import corbin_from_number, corbin_account_sid, corbin_test_auth_token, key_imgbb
 
 
 class Notifications:
@@ -15,26 +17,28 @@ class Notifications:
         self._logger = logging.getLogger('AlarmSystem.notification')
         self._logger.info("Notifications object created")
 
-    def send_alert_message(self):
+    def send_alert_message(self, screen_shot_file):
         # Upload image
-
-        with open("/home/pi/Desktop/3254-20191122051643-snapshot.jpg", "rb") as file:
+        with open(screen_shot_file, "rb") as file:
             url = "https://api.imgbb.com/1/upload"
-            key_imgbb = "98372b96b86a467e80e33905a3418ad4"
+
             payload = {
                 "key": key_imgbb,
                 "image": base64.b64encode(file.read()),
             }
             res = requests.post(url, payload)
-            # TODO - figure out how to get the image uploaded then create the URL
-            print(res)
-        image_url = "https://thumbs.dreamstime.com/z/tv-test-image-card-rainbow-multi-color-bars-geometric-signals-retro-hardware-s-minimal-pop-art-print-suitable-89603635.jpg"
+
+            # get the url linked to the updloaded picture
+            image_url = res.json()["data"]["url"]
+
+        now = datetime.now()
+        now_date_time = now.strftime("%Y-%m-%d %H:%M:%S")
         self.client.messages.create(
-            body="Alert has been detected. View the stream here: http://adamcorbin.com:8081",
+            body="Alert has been detected @" + now_date_time + ". View the stream here: http://adamcorbin.com:8081",
             from_=corbin_from_number,
             to='+17275108407',
-            media_url="https://i.ibb.co/zGD10Gd/150f303418a4.jpg")
+            media_url=image_url)
 
 if __name__ == '__main__':
     notif = Notifications()
-    notif.send_alert_message()
+    notif.send_alert_message("/home/pi/Desktop/3254-20191122051643-snapshot.jpg")
