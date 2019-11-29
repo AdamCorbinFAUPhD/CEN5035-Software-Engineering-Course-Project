a3b2fe935969c31f511e1813bafc8229637d0dcc
Jared Peterson <jpeterson2019@fau.edu>
2019-11-29 15:56:25 -0600

    testing interval ajax calls

---
+++ code/static/js/status.js
@@ -0,0 +1,29 @@
+function fetchdata(){
+                $.ajax({
+                type : 'post'
+                url : '/status',
+            })
+            .done(function(data) {
+                if (data.armed)
+                {
+                    $('#armedTag').text(data.armed);
+                }
+                if (data.is_sensing)
+                {
+                    $('#sensingTag').text(data.is_sensing);
+                }
+                if (data.led_enabled)
+                {
+                    $('#ledEnabledTag').text(data.led_enabled);
+                }
+                if (data.led_color)
+                {
+                    $('#ledColorTag').text(data.led_enabled);
+                }
+            });
+}
+
+
+$(document).ready(function() {
+    setInterval(fetchdata, 1000)
+   });
--- code/ui.py
+++ code/ui.py
@@ -1,11 +1,29 @@
 import urllib
 from time import time, sleep
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 import os
 import sys_client
 
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    client = sys_client.get_client()
+    data = client.get_status()
+    armed = None
+    led_color = None
+    led_enabled = None
+    is_sensing = None
+    if data:
+        armed = data['armed']
+        led_color = data['led_color']
+        led_enabled = data['led_enabled']
+        is_sensing = data["is_sensing"]
+    return render_template("index.html", armed=armed,
+                           led_color=led_color,
+                           led_enabled=led_enabled,
+                           is_sensing=is_sensing)
 
 
 @app.route("/")
@@ -27,11 +45,10 @@ def status():
                            is_sensing=is_sensing)
 
 
-@app.route("/arm_disarm")
+@app.route("/arm_disarm", methods=['POST'])
 def arm_disarm():
     client = sys_client.get_client()
     result = client.arm_disarm("123456")  # This is hard coded since its coming from the UI
-
     return 'Armed' if result["result"] == 'true' else 'Disarmed'
 
 
